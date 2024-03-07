7fa8cf6542e8598cec2ba05c4964ca1d4e377015
Alessandro Zanni <alessandro.zanni.1998@gmail.com>
2024-03-08 00:05:53 +0100

    feat: add roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import { theme } from './theme';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
